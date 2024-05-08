a4b3d4976865d1f5eb26c37dd6572cdba6770f2a
quangtam2003 <165002892+quangtam2003@users.noreply.github.com>
2024-05-08 23:51:03 +0700

    lab3

---
+++ lab3/Core/Inc/input_processing.h
@@ -0,0 +1,21 @@
+/*
+ * input_processing.h
+ *
+ *  Created on: May 8, 2024
+ *      Author: ACER
+ */
+
+#ifndef __INPUT_PROCESSING_H_
+#define __INPUT_PROCESSING_H_
+
+#include "main.h"
+#include "input_reading.h"
+#include "led_display.h"
+#include "timer.h"
+#include "global.h"
+
+void UpdateMode();
+void LedTimeDurationInit();
+void UpdateDurationValue();
+
+#endif /* INC_INPUT_PROCESSING_H_ */
---
+++ lab3/Core/Inc/led_display.h
@@ -0,0 +1,19 @@
+/*
+ * led_display.h
+ *
+ *  Created on: May 8, 2024
+ *      Author: ACER
+ */
+
+#ifndef __LED_DISPLAY_H_
+#define __LED_DISPLAY_H_
+
+#include "main.h"
+#include "timer.h"
+#include "global.h"
+
+void LedDispMode();
+void LEDScanning();
+void resetCountValue();
+
+#endif /* INC_LED_DISPLAY_H_ */
